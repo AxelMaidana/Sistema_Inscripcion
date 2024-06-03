400adf297b0eb9840dcf7f3ed137bf6a62520347
AxelMaidana <maidana.axel2003@gmail.com>
2024-06-03 00:43:02 -0300

    second commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <title>Inscripción de Alumnos</title>
+    <title>Inscripción de Alumnoss</title>
 </head>
 <body class="bg-gray-100 min-h-screen flex flex-col">
     <!-- header -->
